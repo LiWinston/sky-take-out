9bccac787930b345341e841fb61265868d6c7958
lyc <yongchunl@student.unimelb.edu.au>
2024-08-14 17:00:03 +1000

    注解报错改xml
    （唯一一处）

--- sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
+++ sky-server/src/main/java/com/sky/mapper/AddressBookMapper.java
@@ -18,11 +18,6 @@ public interface AddressBookMapper {
      * 新增
      * @param addressBook
      */
-    @Insert("insert into address_book" +
-            "        (user_id, consignee, phone, sex, province_code, province_name, city_code, city_name, district_code," +
-            "         district_name, detail, label, is_default)" +
-            "        values (#{userId}, #{consignee}, #{phone}, #{sex}, #{provinceCode}, #{provinceName}, #{cityCode}, #{cityName}," +
-            "                #{districtCode}, #{districtName}, #{detail}, #{label}, #{isDefault})")
     void insert(AddressBook addressBook);
 
     /**
